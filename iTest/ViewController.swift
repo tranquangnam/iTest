1188fdb26320795f521308b7ad48c98eb5ecb2dc
CO-WELL <namtq6856@co-well.com.vn>
2017-12-21 09:54:55 +0700

    - Init git

---
+++ iTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTest
+//
+//  Created by CO-WELL on 12/21/17.
+//  Copyright © 2017 CO-WELL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
